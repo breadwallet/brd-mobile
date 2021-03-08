07e85ed1c50c88c9fdca27db69650b14322286ef
Ahsan Butt <ahsan.butt@breadwallet.com>
2021-03-08 22:48:44 +0000

    Implement cosmos-bakers-api-client

---
+++ cosmos-bakers-api-client/src/commonTest/kotlin/com/brd/bakerapi/BaketsApiClientTests.kt
@@ -0,0 +1,60 @@
+/**
+ * BreadWallet
+ *
+ * Created by Ahsan Butt <ahsan.butt@breadwallet.com> on 3/4/21.
+ * Copyright (c) 2021 breadwallet LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.brd.bakerapi
+
+import com.brd.bakerapi.models.BakerResult
+import com.brd.bakerapi.BakersApiClient
+import com.brd.bakerapi.models.BakersResult
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class BakersApiClientTests {
+
+    private lateinit var apiClient: BakersApiClient
+
+    @BeforeTest
+    fun before() {
+        apiClient = BakersApiClient.create()
+    }
+
+    @Test
+    fun testGetBakers() = runBlocking {
+        val response = apiClient.getBakers()
+        assertTrue(response is BakersResult.Success, "Error result: $response")
+        assertTrue(response.bakers.isNotEmpty())
+    }
+
+    @Test
+    fun testGetBaker() = runBlocking {
+        val bakersResponse = apiClient.getBakers()
+        assertTrue(bakersResponse is BakersResult.Success, "Error result: $bakersResponse")
+        assertTrue(bakersResponse.bakers.isNotEmpty())
+
+        val bakerAddress = bakersResponse.bakers.first().address
+        val bakerResponse = apiClient.getBaker(bakerAddress)
+        assertTrue(bakerResponse is BakerResult.Success, "Failure result: $bakerResponse")
+    }
+}
