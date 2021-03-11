08241936956da4ea7b1e17f4963e92c19658056b
Drew Carlson <awcarlson@protonmail.com>
2021-03-11 13:45:21 -0800

    Update android gradle plugin to 7.0.0-alpha09

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ val kotlinVersion = "1.4.31"
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("com.android.tools.build:gradle:7.0.0-alpha08")
+    implementation("com.android.tools.build:gradle:7.0.0-alpha09")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-plugins:1.4.9")
     implementation("org.json:json:20201115")
