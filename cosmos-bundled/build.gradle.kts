1ac7eacfde2950071b25d3d023cc33adc6f83960
michael <michael.inger@breadwallet.com>
2021-03-18 22:35:00 +0000

    iOS build issues fixes - Cosmos.framework to xcode-frameworks

--- cosmos-bundled/build.gradle.kts
+++ cosmos-bundled/build.gradle.kts
@@ -36,3 +36,17 @@ kotlin {
         }
     }
 }
+
+val packForXcode by tasks.creating(Sync::class) {
+    group = "build"
+    val mode = System.getenv("CONFIGURATION")?.split("-")?.first() ?: "DEBUG"
+    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+    val framework = kotlin.targets.getByName<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+    dependsOn(framework.linkTask)
+    val targetDir = File(buildDir, "xcode-frameworks")
+    from({ framework.outputDirectory })
+    into(targetDir)
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
