cc96d58851f30295a58cb216a61ed0d202cec5b4
Amit Goel <amit.goel@breadwallet.com>
2021-08-27 17:31:01 -0400

    Release-4.12.0.4

--- buildSrc/src/main/kotlin/brd/BrdRelease.kt
+++ buildSrc/src/main/kotlin/brd/BrdRelease.kt
@@ -25,7 +25,7 @@ object BrdRelease {
     private val engineering = ciTag?.get(2) ?: 0
 
     /** Build version. Increase for each new build. Maximum value: 999 */
-    private val build = ciTag?.lastOrNull() ?: 3
+    private val build = ciTag?.lastOrNull() ?: 4
 
     init {
         check(marketing in 0..99)
