728dff938242fe177f5a64433395020861da78ec
Drew Carlson <awcarlson@protonmail.com>
2021-09-03 11:56:07 -0700

    Update walletkit

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("com.android.tools.build:gradle:7.0.0")
+    implementation("com.android.tools.build:gradle:7.0.2")
     implementation("org.json:json:20201115")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
 }
