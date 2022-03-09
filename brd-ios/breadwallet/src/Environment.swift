f40ded9232e26aba629d2eb52bbd2a7f4f34541c
rcresnik <rok.cresnik@equaleyes.com>
2022-03-09 08:25:28 +0100

    chore: extract base url to env variable.

--- brd-ios/breadwallet/src/Environment.swift
+++ brd-ios/breadwallet/src/Environment.swift
@@ -117,11 +117,18 @@ struct E {
         return String(os.majorVersion) + "." + String(os.minorVersion) + "." + String(os.patchVersion)
     }()
     
-    static var apiToken: String? {
+    static var apiToken: String {
         guard let token = Bundle.main.object(forInfoDictionaryKey: "API_TOKEN") as? String else {
-            return nil
+            fatalError("Env not configured properly")
         }
         return token
+    }
+    
+    static var apiUrl: String {
+        guard let url = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+            fatalError("Env not configured properly")
+        }
+        return url
     }
     
 }
